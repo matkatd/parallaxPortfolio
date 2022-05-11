636cbc9a249fc2601a34e9fa14635b4729d5a19d
David T <matkatdavid@gmail.com>
2022-05-11 10:59:44 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import { Rellax } from "./rellax.min.js";
+
+const rellax = new Rellax(".rellax", {
+  center: true,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false,
+});
